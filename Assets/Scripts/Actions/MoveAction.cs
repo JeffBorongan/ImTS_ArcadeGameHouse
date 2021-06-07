2fab2516fbadc2f2f9c778d24e2689386b0d198e
JeffBorongan <jeffborongan@gmail.com>
2021-06-07 15:57:07 +0800

    [Space Ranger - Jeff] - Made the Script to modify Captain Rogers' Behavior

--- Assets/Scripts/Actions/MoveAction.cs
+++ Assets/Scripts/Actions/MoveAction.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Move Action", menuName = "Action/Move", order = 2)]
 public class MoveAction : Action
 {
-
+    public Transform targetPosition = null;
 }
--- Assets/Scripts/Actor/Actor.cs
+++ Assets/Scripts/Actor/Actor.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,20 +6,34 @@ using UnityEngine.Events;
 
 public class Actor : MonoBehaviour
 {
+    AudioSource audioSource;
     public UnityEvent OnEndAction = new UnityEvent();
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void ExecuteAction(Action action)
     {
 
     }
 
-    public void MoveTo(Transform targetPosition)
+    public void MoveToLocation(Transform targetPosition, float travelDuration, Ease easeType)
     {
-
+        transform.DOMove(targetPosition.position, travelDuration).SetEase(easeType).OnComplete(OnEndAction.Invoke);
     }
 
     public void Speak(AudioClip clip)
     {
+        audioSource.clip = clip;
+        audioSource.Play();
+        StartCoroutine(InvokeOnEndClip(audioSource));
+    }
 
+    IEnumerator InvokeOnEndClip(AudioSource clip)
+    { 
+        yield return new WaitUntil(() => !clip.isPlaying);
+        OnEndAction.Invoke();
     }
 }
