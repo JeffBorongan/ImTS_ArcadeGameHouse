14896cdb2fad34e87a933f9d4ce53030b3271d05
PrinceEgo <barino.jm@gmail.com>
2021-07-06 10:34:58 +0800

    VR Character

---
+++ Assets/Scripts/VR/VRLowerBodyAnimator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLowerBodyAnimator : MonoBehaviour
+{
+    [SerializeField] private float speedTreshold = 0.1f;
+    [Range(0, 1)]
+    [SerializeField] private float smoothing = 1f;
+    private VRRig vRRig = null;
+    private Animator animator = null;
+    private Vector3 previousPos = Vector3.zero;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        vRRig = GetComponent<VRRig>();
+        previousPos = vRRig.head.vrTarget.position;
+    }
+
+    private void Update()
+    {
+        Vector3 headsetSpeed = (vRRig.head.vrTarget.position - previousPos) / Time.deltaTime;
+        headsetSpeed.y = 0f;
+
+        Vector3 headsetLocalSpeed = transform.InverseTransformDirection(headsetSpeed);
+        previousPos = vRRig.head.vrTarget.position;
+
+        float previousBlendX = animator.GetFloat("BlendX");
+        float previousBlendY = animator.GetFloat("BlendY");
+
+        animator.SetFloat("BlendX", Mathf.Lerp(previousBlendX, Mathf.Clamp(headsetLocalSpeed.x, -1f, 1f), smoothing));
+        animator.SetFloat("BlendY", Mathf.Lerp(previousBlendY, Mathf.Clamp(headsetLocalSpeed.z, -1f, 1f), smoothing));
+    }
+
+
+}
---
+++ Assets/Scripts/VR/VRRig.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRRig : MonoBehaviour
+{
+    public Transform headConstaint;
+    public Vector3 headBodyOffset;
+
+    public VRMap head;
+    public VRMap leftHand;
+    public VRMap rightHand;
+
+    private void Start()
+    {
+        headBodyOffset = transform.position - headConstaint.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = headConstaint.position + headBodyOffset;
+        transform.forward = Vector3.ProjectOnPlane(headConstaint.up, Vector3.up).normalized;
+        head.Map();
+        leftHand.Map();
+        rightHand.Map();
+    }
+}
+
+[System.Serializable]
+public class VRMap
+{
+    public Transform vrTarget;
+    public Transform rigTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+
+    public void Map()
+    {
+        rigTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+
+}
