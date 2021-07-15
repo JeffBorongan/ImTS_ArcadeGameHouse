f6a708091ed9e28490ecb368e0635cecfbdc750c
PrinceEgo <barino.jm@gmail.com>
2021-07-15 13:25:48 +0800

    Room Mimic Test

--- Assets/Scripts/VR/VRRigMimic.cs
+++ Assets/Scripts/VR/VRRigMimic.cs
@@ -22,8 +22,8 @@ public class VRRigMimic : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //transform.position = (headConstaint.position + headBodyOffset) + bodyOffset;
-        //transform.forward = Vector3.ProjectOnPlane(headConstaint.up, Vector3.up).normalized;
+        transform.position = (headConstaint.position + headBodyOffset) + bodyOffset;
+        transform.forward = Vector3.ProjectOnPlane(headConstaint.up, Vector3.up).normalized;
         head.Map();
         leftHand.Map();
         rightHand.Map();
@@ -37,11 +37,11 @@ public class VRRigMimic : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = headColorGizmos;
-        Gizmos.DrawSphere(head.vrTarget.TransformPoint(head.trackingPositionOffset), 0.1f);
+        Gizmos.DrawSphere(head.GetRigPosition(), 0.1f);
         Gizmos.color = leftHandColorGizmos;
-        Gizmos.DrawSphere(leftHand.vrTarget.TransformPoint(leftHand.trackingPositionOffset), 0.1f);
+        Gizmos.DrawSphere(leftHand.GetRigPosition(), 0.1f);
         Gizmos.color = rightHandColorGizmos;
-        Gizmos.DrawSphere(rightHand.vrTarget.TransformPoint(rightHand.trackingPositionOffset), .1f);
+        Gizmos.DrawSphere(rightHand.GetRigPosition(), .1f);
     }
 }
 
@@ -51,14 +51,20 @@ public class VRMapMimic
     public Transform vrTarget;
     public Transform rigTarget;
     public Vector3 trackingPositionOffset;
+    public Vector3 trackingGlobalPositionOffset;
     public Vector3 trackingRotationOffset;
     public bool inversePosition = false;
     public bool inverseRotation = false;
 
     public void Map()
     {
-        rigTarget.position = inversePosition ? vrTarget.TransformPoint(trackingPositionOffset) :  vrTarget.TransformPoint(trackingPositionOffset);
+        rigTarget.position = trackingGlobalPositionOffset + (inversePosition ? vrTarget.TransformPoint(trackingPositionOffset) :  vrTarget.TransformPoint(trackingPositionOffset));
         rigTarget.rotation = inversePosition ? Quaternion.Inverse(vrTarget.rotation * Quaternion.Euler(trackingRotationOffset)) : vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+
+    public Vector3 GetRigPosition()
+    {
+        return trackingGlobalPositionOffset + (inversePosition ? vrTarget.TransformPoint(trackingPositionOffset) : vrTarget.TransformPoint(trackingPositionOffset));
     }
 
 }
