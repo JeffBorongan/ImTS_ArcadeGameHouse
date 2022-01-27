1bbbfd4bbe78bd466b0c4e6f5443bac78e6613ea
Exodia Jeff <jeff@exodiagamedev.com>
2022-01-27 17:40:09 +0800

    [UP Cerebral Palsy - Jeff] - Implemented Nav Mesh Boundaries and other changes

--- Assets/Elevator Assets/Scripts/SquatGameManagement.cs
+++ Assets/Elevator Assets/Scripts/SquatGameManagement.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using DG.Tweening;
+using UnityEngine.AI;
+using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
+using UnityEngine.UI;
 
 public class SquatGameManagement : GameManagement
 {
@@ -30,9 +34,10 @@ public class SquatGameManagement : GameManagement
 
     [Header("Enemy Spawning")]
     [SerializeField] private List<Transform> enemySpawnPoints = new List<Transform>();
-    [SerializeField] private List<Transform> enemyGoToPoints = new List<Transform>();
+    [SerializeField] private List<Transform> enemyDestinationPoints = new List<Transform>();
+    private IEnumerator spawningCour = null;
     private bool isSpawning = false;
-    private bool proceedToNext = false;
+    private bool proceedToNextSpawn = false;
 
     [Header("Doors and Lights")]
     [SerializeField] private List<GameObject> doors = new List<GameObject>();
@@ -46,7 +51,14 @@ public class SquatGameManagement : GameManagement
     [SerializeField] private GameObject leftLever;
     [SerializeField] private GameObject rightLever;
 
+    [Header("UI")]
+    [SerializeField] private Image imgTimerIcon = null;
+    [SerializeField] private TextMeshProUGUI txtCountdownTimer = null;
+    private IEnumerator countdownTimerCour = null;
+
     private AlienMovement alien = null;
+    private IEnumerator enemyCheckingCour = null;
+    private int currentEnemyReachedTheDoor = 0;
     private int index = 0;
 
     #endregion
@@ -55,20 +67,7 @@ public class SquatGameManagement : GameManagement
 
     public override void InitializeGame()
     {
-        //StartGame(new SquatGameSessionData(), () => { });
-
-        foreach (var door in doors)
-        {
-            door.transform.DOMoveY(-1.1f, 0.5f);
-        }
-
-        foreach (var light in lights)
-        {
-            light.GetComponent<Renderer>().material = redLight;
-        }
-
-        isSpawning = true;
-        StartCoroutine(SpawningCour());
+        StartGame(new SquatGameSessionData(), () => { });
     }
 
     #endregion
@@ -82,26 +81,64 @@ public class SquatGameManagement : GameManagement
         btnStartGame.onClick.AddListener(() =>
         {
             sessionData = (SquatGameSessionData)data;
+            countdownTimerCour = TimeCour(3, txtCountdownTimer, () =>
+            {
+                imgTimerIcon.gameObject.SetActive(true);
+
+                foreach (var door in doors)
+                {
+                    door.transform.DOMoveY(-1.1f, 0.5f);
+                }
+
+                foreach (var light in lights)
+                {
+                    light.GetComponent<Renderer>().material = redLight;
+                }
+
+                isSpawning = true;
+                spawningCour = SpawningCour();
+                StartCoroutine(spawningCour);
+
+                enemyCheckingCour = EnemyCheckingCour(() =>
+                {
+                    StopGame();
+                    OnEndGame.Invoke();
+                    OnGameEnd.Invoke();
+                });
+
+                StartCoroutine(enemyCheckingCour);
+            });
+
+            StartCoroutine(countdownTimerCour);
             pnlStartGame.gameObject.SetActive(false);
-
-            /*foreach (var door in doors)
-            {
-                door.transform.DOMoveY(-1.1f, 0.5f);
-            }
-
-            foreach (var light in lights)
-            {
-                light.GetComponent<Renderer>().material = redLight;
-            }
-
-            isSpawning = true;
-            StartCoroutine(SpawningCour());*/
         });
     }
 
     public override void StopGame()
     {
+        isSpawning = false;
 
+        foreach (var door in doors)
+        {
+            door.transform.DOMoveY(1f, 0.5f);
+        }
+
+        foreach (var light in lights)
+        {
+            light.GetComponent<Renderer>().material = redLight;
+        }
+
+        StopCoroutine(enemyCheckingCour);
+        StopCoroutine(spawningCour);
+    }
+
+    #endregion
+
+    #region Game Update
+
+    private void Update()
+    {
+        UpdateLeverPulling(isSpawning, sessionData.playerLeverLift, sessionData.playerLeverDrop);
     }
 
     #endregion
@@ -112,95 +149,98 @@ public class SquatGameManagement : GameManagement
     {
         while (isSpawning)
         {
-            SpawnEnemy(enemySpawnPoints[index], enemyGoToPoints[index]);
-            yield return new WaitUntil(() => proceedToNext);
+            SpawnEnemy(enemySpawnPoints[index], enemyDestinationPoints[index]);
+            yield return new WaitUntil(() => proceedToNextSpawn);
         }
     }
 
     private void SpawnEnemy(Transform spawnPoint, Transform enemyGoal)
     {
-        proceedToNext = false;
+        proceedToNextSpawn = false;
         GameObject clone = ObjectPooling.Instance.GetFromPool(TypeOfObject.EnemyAlien);
         clone.transform.position = spawnPoint.position;
         clone.transform.rotation = spawnPoint.rotation;
         clone.SetActive(true);
 
         alien = clone.GetComponent<AlienMovement>();
-        alien.SetMovementSpeed(0.5f);
+        alien.SetMovementSpeed(sessionData.enemySpeed);
         Vector3 targetDestination = new Vector3(alien.transform.position.x, alien.transform.position.y, enemyGoal.position.z);
         alien.AlienAgent.SetDestination(targetDestination);
 
         alien.OnDeath.AddListener(() =>
         {
-            if (isSpawning && index == 4)
-            {
-                index = 0;
-                foreach (var door in doors)
-                {
-                    door.transform.DOMoveY(-1.1f, 0.5f);
-                }
-
-                foreach (var light in lights)
-                {
-                    light.GetComponent<Renderer>().material = redLight;
-                }
-            }
-            else
-            {
-                index++;
-            }
-
-            alien.gameObject.SetActive(false);
-            proceedToNext = true;
             alien.OnDeath.RemoveAllListeners();
         });
 
         alien.OnReachDestination.AddListener(() =>
         {
-            foreach (var door in doors)
-            {
-                door.transform.DOMoveY(1f, 0.5f);
-            }
-
-            foreach (var light in lights)
-            {
-                light.GetComponent<Renderer>().material = redLight;
-            }
-
-            isSpawning = false;
-            StopCoroutine(SpawningCour());
+            AddEnemyReachedTheDoor();
             alien.OnReachDestination.RemoveAllListeners();
         });
     }
 
     #endregion
 
+    #region Enemy Status Checking
+
+    IEnumerator EnemyCheckingCour(UnityAction OnEndGame)
+    {
+        yield return new WaitUntil(() => currentEnemyReachedTheDoor >= sessionData.enemyReachedTheDoor);
+        OnEndGame.Invoke();
+    }
+
+    private void AddEnemyReachedTheDoor()
+    {
+        currentEnemyReachedTheDoor++;
+        if (currentEnemyReachedTheDoor == sessionData.enemyReachedTheDoor)
+        {
+            StopGame();
+            OnGameEnd.Invoke();
+        }
+    }
+
+    #endregion
+
     #region Lever Pulling
 
-    private void UpdateLeverPulling(bool engageLever, Vector3 leftLever, Vector3 rightLever, float maximumLift, float minimumDrop)
+    private void UpdateLeverPulling(bool engageLever, float maximumLift, float minimumDrop)
     {
+        leftLever.GetComponent<XRGrabInteractable>().enabled = engageLever;
+        rightLever.GetComponent<XRGrabInteractable>().enabled = engageLever;
+
         if (engageLever)
         {
-            if (leftLever.y >= maximumLift && rightLever.y >= maximumLift)
+            if (leftLever.transform.position.y <= minimumDrop && rightLever.transform.position.y <= minimumDrop)
             {
                 if (moveStatus == MoveStatus.Half)
                 {
                     isDoorMovable = true;
-                    doors[index].transform.DOMoveY(0f, 0.5f);
+                    doors[index].transform.DOMoveY(sessionData.doorHalfCloseDistance, sessionData.doorCloseSpeed);
                 }
                 if (moveStatus == MoveStatus.Whole)
                 {
                     isDoorMovable = false;
+                    doors[index].GetComponent<NavMeshObstacle>().enabled = true;
                     lights[index].GetComponent<Renderer>().material = greenLight;
-                    doors[index].transform.DOMoveY(1f, 0.5f).OnComplete(() =>
+                    doors[index].transform.DOMoveY(sessionData.doorFullCloseDistance, sessionData.doorCloseSpeed).OnComplete(() =>
                     {
-                        alien.OnDeath.Invoke();
+                        if (isSpawning && index == 4)
+                        {
+                            StopGame();
+                            OnGameEnd.Invoke();
+                        }
+                        else
+                        {
+                            index++;
+                        }
+
+                        proceedToNextSpawn = true;
                     });
                     moveStatus = MoveStatus.None;
                 }
             }
 
-            if (leftLever.y <= minimumDrop && rightLever.y <= minimumDrop)
+            if (leftLever.transform.position.y >= maximumLift && rightLever.transform.position.y >= maximumLift)
             {
                 if (!isDoorMovable)
                 {
@@ -216,11 +256,23 @@ public class SquatGameManagement : GameManagement
 
     #endregion
 
-    #region Update
+    #region Timer
 
-    private void Update()
+    IEnumerator TimeCour(int timerDuration, TextMeshProUGUI txt, UnityAction OnEndTimer, bool inMinutes = false)
     {
-        UpdateLeverPulling(isSpawning, leftLever.transform.position, rightLever.transform.position, 1f, 0.3f);
+        txt.gameObject.SetActive(true);
+        int currentTime = timerDuration;
+        while (currentTime >= 0)
+        {
+            int minutes = currentTime / 60;
+            int seconds = currentTime - (minutes * 60);
+            txt.text = inMinutes ? minutes + ":" + seconds : currentTime.ToString();
+            yield return new WaitForSeconds(1f);
+            currentTime--;
+        }
+
+        txt.gameObject.SetActive(false);
+        OnEndTimer.Invoke();
     }
 
     #endregion
@@ -230,6 +282,11 @@ public class SquatGameSessionData : SessionData
 {
     public float enemySpeed = 0.5f;
     public int enemyReachedTheDoor = 1;
+    public float playerLeverLift = 1f;
+    public float playerLeverDrop = 0.3f;
+    public float doorHalfCloseDistance = 0f;
+    public float doorFullCloseDistance = 1f;
+    public float doorCloseSpeed = 0.5f;
 }
 
 public enum MoveStatus
