e5f3eaa6614e89ca917ac5e6d0affd4a1bebf3be
bgalecio-ImTS <bcgalecio@up.edu.ph>
2022-06-20 17:50:11 +0800

    Working Semi Cave - No Teleport

---
+++ Assets/Scripts/SemiCave/FollowPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPosition : MonoBehaviour
+{
+    [SerializeField] private GameObject trackedObject;
+    [SerializeField] private bool trackX;
+    [SerializeField] private float xOffset;
+
+    [SerializeField] private bool trackY;
+    [SerializeField] private float yOffset;
+
+    [SerializeField] private bool trackZ;
+    [SerializeField] private float zOffset;
+
+    float x, y, z;
+
+    void Update()
+    {
+        if (trackX) x = trackedObject.transform.localPosition.x;
+        else x = transform.localPosition.x;
+        if (trackY) y = trackedObject.transform.localPosition.y;
+        else y = transform.localPosition.y;
+        if (trackZ) z = trackedObject.transform.localPosition.z;
+        else z = transform.localPosition.z;
+
+        x += xOffset;
+        y += yOffset;
+        z += zOffset;
+
+        transform.localPosition = new Vector3(x,y,z);
+    }
+}
